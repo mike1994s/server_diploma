bac552d6ad51f50969324e9697f12d03180530ac
Misha <meshuin94@gmail.com>
2016-04-26 21:10:55 +0400

    add source vk and fb

--- token/test/app/routes.js
+++ token/test/app/routes.js
@@ -17,13 +17,8 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
-    app.get('/start', isLoggedIn, function(req, res){
-	res.render('chat.ejs', {
-		user : req.user
-	});
-    });
 
-    app.get('/chat', function(req, res){
+    app.get('/chat',isLogin,  function(req, res){
 	res.render('chat.ejs');	
    });
     // =====================================
@@ -99,6 +94,45 @@ app.get('/tokenchat',
 		 // newUser.vk.token = token; // we will save the token that facebook provides to the user                    
 
 var User = require('../app/models/user');
+var request = require('request');
+function isLogin(req, res, next){
+	var source = req.param('source');
+	var token  = req.param('token');
+	if (source == "vk"){
+		var uri = 'https://api.vk.com/method/secure.checkToken?token=' + token;
+		request(uri, function (error, response, body) {
+			if (error){
+				console.dir(error);
+			}
+			var ans  = JSON.parse(body);
+			console.log("was not  error " + !error);
+	
+			var error_code = ans.error.error_code;
+			console.log(error_code + '   = error code ');
+			if (!error && error_code != 5) {
+    				return next(); 
+ 			 }
+});
+}
+else if (source == "fb"){
+		var uri = 'https://graph.facebook.com/me?access_token=' + token;
+		request(uri, function (error, response, body) {
+			if (error){
+				console.dir(error);
+			}
+			var ans  = JSON.parse(body);
+			console.log("was error not " + !error);
+			var error_code = ans.error.code;
+			console.log(error_code + '   = error code ');
+		console.dir(body);
+		if (!error && error_code != 190) {
+    			return next(); 
+ 		 }
+	});
+		
+	}
+	res.json({code : 401});
+}
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
--- token/test/socket/index.js
+++ token/test/socket/index.js
@@ -2,14 +2,14 @@
 
 module.exports = function(server, middle){
  	var io = require('socket.io')(server);
-	var User  = require('../app/models/user');
+/*	var User  = require('../app/models/user');
 	io.use(function(socket, next){
        	 // Wrap the express middleware
         	middle(socket.request, {}, next);
 	}); 
-
+ */
  	io.on('connection', function(socket){
-		console.log(socket.request);
+/*		console.log(socket.request);
   		console.dir(socket.request.session.passport)
   		var id = socket.request.session.passport.user;
 		User.findById(id, function(err, user) {
@@ -19,7 +19,7 @@ module.exports = function(server, middle){
 				return;
 			console.dir(user);
            	  	socket.username =  user.vk.name || user.facebook.email;
-        	});
+        	});*/
 		socket.on('chat message', function(msg){
   	 	 	io.emit('chat message', socket.username + ":" + msg);
  	 	});
